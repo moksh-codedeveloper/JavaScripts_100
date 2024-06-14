4f17ca9c5d7fba6ca8efe08c059b935fcf2d8201
MokshMalde <maldemoksh437@gmail.com>
2024-06-14 11:22:41 +0530

    new project for js test file

---
+++ 01_basicsjs/01Variables.js
@@ -0,0 +1,5 @@
+let accountId = 12334;
+const accountName = "Moksh Malde";
+let accountName2 = "Moksh Haria";
+let accountName3 = "Moksh Solanki";
+console.table([accountId, accountName, accountName2, accountName3]); // provides a tabular structure used for multiple prints at a time
---
+++ 02dataTypes/02dataTypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all code as a newer version of js 
+/* [IMP note] :-
+we can not use alert function in node because it has a different syntax 
+alert(3+3) this syntax is invalid  for browser it is valid 
+*/
